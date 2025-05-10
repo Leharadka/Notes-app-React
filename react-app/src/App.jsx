13502f8596a93f6014bd4d858236833771207cd8
Aleksei <alex.porfirov@gmail.com>
2025-05-10 15:03:11 +0300

    Инциализация + добавление первого компонента

---
+++ react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Button from './components/Button/Button'
+
+function App() {
+
+  // return React.createElement('div', {}, 'Проект');
+  
+  return (
+    <>
+      <h1>Какой-то текст</h1>
+      <p>Заголовок</p>
+      <Button/>
+    </>
+  )
+}
+
+export default App
---
+++ react-app/src/components/Button/Button.jsx
@@ -0,0 +1,8 @@
+function Button() {
+ 
+  return (
+     <button>Сохранить</button>
+  )
+}
+
+export default Button
